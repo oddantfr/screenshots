0d100a0800f9adf6e7201154b0bc59315e8e1bef
oddantfr <oddantsounds@gmail.com>
2023-08-28 23:04:33 +0200

    range

--- src/app-shell/app-shell.ts
+++ src/app-shell/app-shell.ts
@@ -18,7 +18,8 @@ import {
 } from '@material/material-color-utilities';
 import {getArgbValuesFromImage} from '../utils.js';
 import {deleted} from '../deleted.js';
-import { withController } from '@snar/lit';
+import {withController} from '@snar/lit';
+import {settings, settingsDialog} from '../settings-dialog.js';
 // import styles from './app-shell.css?inline';
 
 @customElement('app-shell')
@@ -38,28 +39,39 @@ export class AppShell extends LitElement {
 		const imgSrc = images[this.imgIndex];
 		return html`
 			<div class="flex justify-between items-center p-2">
-				<md-filled-icon-button @click=${this.#backward}>
-					<md-icon>arrow_backward</md-icon>
-				</md-filled-icon-button>
 				<md-filled-icon-button @click=${this.#random}>
 					<md-icon>casino</md-icon>
 				</md-filled-icon-button>
-				<span>index: ${this.imgIndex}</span>
-				<md-filled-icon-button @click=${this.#delete}>
+				<md-filled-icon-button @click=${this.#delete}
+				style="--md-filled-icon-button-container-color:var(--md-sys-color-error)"
+				>
 					<md-icon
+					style="color:var(--md-sys-color-on-error)"
 						>${deleted.exists(images[this.imgIndex])
 							? 'delete_forever'
 							: 'delete'}</md-icon
 					>
 				</md-filled-icon-button>
-				<md-filled-icon-button @click=${this.#forward}>
-					<md-icon>arrow_forward</md-icon>
+				<md-filled-icon-button @click=${() => settingsDialog.show()}>
+					<md-icon>settings</md-icon>
 				</md-filled-icon-button>
+				<div class="flex items-center">
+					<md-filled-icon-button @click=${this.#backward}>
+						<md-icon>arrow_backward</md-icon>
+					</md-filled-icon-button>
+					<div style="min-width:100px;" class="text-center">
+						index: ${this.imgIndex}
+					</div>
+					<md-filled-icon-button @click=${this.#forward}>
+						<md-icon>arrow_forward</md-icon>
+					</md-filled-icon-button>
+				</div>
 			</div>
 			<img src="images/${imgSrc}" width="100%" />
 
 			<div class="flex items-center p-2">
 				<color-picker
+					class="mr-3"
 					@input=${(e: Event) => {
 						themeStore.themeColor = (e.target as ColorPicker).value;
 					}}
@@ -87,7 +99,17 @@ export class AppShell extends LitElement {
 	}
 
 	#random() {
-		this.imgIndex = Math.floor(Math.random() * images.length);
+		const total = images.length - 1;
+		const startIndex = Math.floor((total * settings.rangeStart) / 100);
+		const endIndex = Math.ceil((total * settings.rangeEnd) / 100);
+
+		const candidates = images.slice(startIndex, endIndex);
+		if (candidates.length === 0) {
+			candidates.push(images[startIndex]);
+		}
+
+		this.imgIndex =
+			Math.floor(Math.random() * (endIndex - startIndex)) + startIndex;
 	}
 
 	#backward() {
