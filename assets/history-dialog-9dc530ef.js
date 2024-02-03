aa0ab35335fda79e9a9bed47de81101e5f0e22f2
oddantfr <141629939+oddantfr@users.noreply.github.com>
2024-02-03 17:53:39 +0000

    Build: (d01d991) remove preinstall script:

---
+++ assets/history-dialog-9dc530ef.js
@@ -0,0 +1,38 @@
+import{s as v,x as r,b as p,_ as h,e as u,t as g,k as s,N as f,q as y,O as _,j as b,i as w,u as x,w as $}from"./index-be573e0b.js";import"./elevation-561f2ab9.js";class C extends v{render(){return r`
+      <md-elevation part="elevation"></md-elevation>
+      <div class="background"></div>
+      <slot></slot>
+      <div class="outline"></div>
+    `}}const E=p`:host{--_container-color: var(--md-elevated-card-container-color, var(--md-sys-color-surface-container-low, #f7f2fa));--_container-elevation: var(--md-elevated-card-container-elevation, 1);--_container-shadow-color: var(--md-elevated-card-container-shadow-color, var(--md-sys-color-shadow, #000));--_container-shape: var(--md-elevated-card-container-shape, 12px)}/*# sourceMappingURL=elevated-styles.css.map */
+`;const O=p`:host{border-radius:var(--_container-shape);box-sizing:border-box;display:flex;flex-direction:column;position:relative;z-index:0}md-elevation,.background,.outline{border-radius:inherit;inset:0;pointer-events:none;position:absolute}.background{background:var(--_container-color);z-index:-1}.outline{border:1px solid rgba(0,0,0,0);z-index:1}md-elevation{z-index:-1;--md-elevation-level: var(--_container-elevation);--md-elevation-shadow-color: var(--_container-shadow-color)}slot{border-radius:inherit}/*# sourceMappingURL=shared-styles.css.map */
+`;let c=class extends C{};c.styles=[O,E];c=h([u("md-elevated-card")],c);const P="";var D=Object.defineProperty,L=Object.getOwnPropertyDescriptor,m=(e,o,t,n)=>{for(var a=n>1?void 0:n?L(o,t):o,l=e.length-1,d;l>=0;l--)(d=e[l])&&(a=(n?d(o,t,a):d(a))||a);return n&&a&&D(o,t,a),a};let i=class extends v{constructor(){super(...arguments),this.open=!1}firstUpdated(){s.bind(this)}render(){return r`
+			<md-dialog ?open=${this.open} @close=${()=>this.open=!1}>
+				<header slot="headline"><md-icon>${f}</md-icon> History</header>
+
+				<form slot="content" method="dialog" id="form">
+					${s.history.length>0?r`
+								${s.history.map(e=>r`
+										<md-elevated-card
+											class="my-10"
+											@click=${()=>{s.currentIndex=e,this.dialog.close()}}
+										>
+											<md-filled-tonal-icon-button
+												form
+												@click=${async o=>{o.stopPropagation();try{const{materialConfirm:t}=await y(()=>import("./index-88ec65b2.js"),["./index-88ec65b2.js","./index-be573e0b.js"],import.meta.url);await t(),s.removeIndex(e)}catch{}}}
+												class="absolute -top-1 right-0"
+												><md-icon>${_}</md-icon></md-filled-tonal-icon-button
+											>
+											<img
+												src="./images/${b[e]}"
+												class="w-full rounded-xl"
+											/>
+										</md-elevated-card>
+									`)}
+							`:r` <p class="text-center">Empty history</p> `}
+				</form>
+
+				<div slot="actions">
+					<md-text-button form="form">Close</md-text-button>
+				</div>
+			</md-dialog>
+		`}async show(){if(this.dialog.open){const e=new Promise(o=>{const t=()=>{o(null),this.dialog.removeEventListener("closed",t)};this.dialog.addEventListener("closed",t)});this.dialog.close(),await e}this.open=!0}};m([g()],i.prototype,"open",2);m([w("md-dialog")],i.prototype,"dialog",2);i=m([x({name:"history-dialog",inject:!0}),$(P)],i);const M=window.historyDialog=new i;export{M as historyDialog};
---
+++ assets/index-88ec65b2.js
@@ -0,0 +1,53 @@
+import{P as h,Q as C,R as g,S as E}from"./index-be573e0b.js";const A=Symbol.for(""),S=l=>{if((l==null?void 0:l.r)===A)return l==null?void 0:l._$litStatic$},v=l=>({_$litStatic$:l,r:A}),m=(l,...s)=>({_$litStatic$:s.reduce((t,e,n)=>t+(c=>{if(c._$litStatic$!==void 0)return c._$litStatic$;throw Error(`Value passed to 'literal' function must be a 'literal' result: ${c}. Use 'unsafeStatic' to pass non-literal values, but
+            take care to ensure page security.`)})(e)+l[n+1],l[0]),r:A}),_=new Map,U=l=>(s,...t)=>{const e=t.length;let n,c;const r=[],p=[];let f,u=0,d=!1;for(;u<e;){for(f=s[u];u<e&&(c=t[u],(n=S(c))!==void 0);)f+=n+s[++u],d=!0;u!==e&&p.push(c),r.push(f),u++}if(u===e&&r.push(s[e]),d){const b=r.join("$$lit$$");(s=_.get(b))===void 0&&(r.raw=r,_.set(b,s=r)),t=p}return l(s,...t)},T=U(h);const M={ATTRIBUTE:1,CHILD:2,PROPERTY:3,BOOLEAN_ATTRIBUTE:4,EVENT:5,ELEMENT:6},O=l=>(...s)=>({_$litDirective$:l,values:s});let j=class{constructor(s){}get _$AU(){return this._$AM._$AU}_$AT(s,t,e){this._$Ct=s,this._$AM=t,this._$Ci=e}_$AS(s,t){return this.update(s,t)}update(s,t){return this.render(...t)}};const P="important",I=" !"+P,w=O(class extends j{constructor(l){var s;if(super(l),l.type!==M.ATTRIBUTE||l.name!=="style"||((s=l.strings)==null?void 0:s.length)>2)throw Error("The `styleMap` directive must be used in the `style` attribute and must be the only part in the attribute.")}render(l){return Object.keys(l).reduce((s,t)=>{const e=l[t];return e==null?s:s+`${t=t.includes("-")?t:t.replace(/(?:^(webkit|moz|ms|o)|)(?=[A-Z])/g,"-$&").toLowerCase()}:${e};`},"")}update(l,[s]){const{style:t}=l.element;if(this.ut===void 0)return this.ut=new Set(Object.keys(s)),this.render(s);for(const e of this.ut)s[e]==null&&(this.ut.delete(e),e.includes("-")?t.removeProperty(e):t[e]=null);for(const e in s){const n=s[e];if(n!=null){this.ut.add(e);const c=typeof n=="string"&&n.endsWith(I);e.includes("-")||c?t.setProperty(e,c?n.slice(0,-11):n,c?P:""):t[e]=n}}return C}});function x({headline:l,content:s,cancelButton:t,confirmButton:e,blockScrimClick:n=!1,blockEscapeKey:c=!1,onDialogReady:r,styles:p}){return new Promise(async(f,u)=>{const d=document.createElement("div");let b=Promise.resolve(),y=Promise.resolve(),o=!1;g(h`
+				<md-dialog
+					?block-scrim-click="${n}"
+					?block-escape-key="${c}"
+					style="${w(p??{})}"
+					@cancel=${a=>{const $=a.target;$.returnValue===""&&(o?$.hasAttribute("block-escape-key")&&a.preventDefault():$.hasAttribute("block-scrim-click")&&a.preventDefault(),o=!1)}}
+					@keydown=${a=>{a.code==="Escape"&&(o=!0)}}
+					@closed=${async a=>{switch(a.target.returnValue){case"":case"cancel":u(await b);break;case"confirm":default:f(await y)}a.target.remove(),d.remove()}}
+				>
+				</md-dialog>
+			`,d),document.body.prepend(d);const i=d.querySelector(":scope > md-dialog");await i.updateComplete,g(h`
+				<div slot="headline">${l}</div>
+				<form method="dialog" id="inner-form" slot="content">
+					${s(i)}
+				</form>
+
+				${t||e?h`
+							<div slot="actions">
+								${t?(()=>{t.buttonType=t.buttonType??"md-text-button";const a=m`${v(t.buttonType)}`;return T`
+									<${a}
+										id="cancelButton"
+										style=${w(t.styles??{})}
+										@click=${()=>{t.callback&&(b=new Promise(async $=>{$(await t.callback(i))}))}}
+										form="inner-form"
+										value="${t.dialogAction??"cancel"}"
+										>${t.label??"Cancel"}</${a}
+									>
+								`})():E}
+								${e?(()=>{e.buttonType=e.buttonType??"md-text-button";const a=m`${v(e.buttonType)}`;return T`
+									<${a}
+										id="confirmButton"
+										style=${w(e.styles??{})}
+										@click=${()=>{e.callback&&(y=new Promise(async $=>{$(await e.callback(i))}))}}
+										form="inner-form"
+										value="${e.dialogAction??"confirm"}"
+										>${e.label??"Confirm"}</${a}
+									>
+								`})():E}
+							</div>
+					  `:null}
+			`,i),i.$={confirmButton:null,cancelButton:null},i.querySelectorAll("[id]").forEach(a=>{i.$[a.getAttribute("id")]=a}),await i.updateComplete,await(r==null?void 0:r(i)),i.show()})}async function R({promptText:l,initialValue:s,textfieldType:t,confirmButton:e,blockEscapeKey:n,blockScrimClick:c,autocomplete:r,type:p,rows:f,cols:u,dialogStyles:d,forbiddenValues:b,forbiddenText:y}={}){return r!==""&&!r&&(r="off"),r===!0&&(r=""),await x({headline:l??"Enter a value",blockEscapeKey:n,blockScrimClick:c,styles:d,content(o){const i=t?m`${v(t)}`:m`md-filled-text-field`;return T`<${i}
+				id="textfield"
+				style="width:100%"
+				autofocus
+				autocomplete=${r}
+				type=${p??"input"}
+				rows=${f??3}
+				cols=${u??20}
+				@keyup=${()=>{const a=o.$.textfield,$=a.value,k=b&&b.includes($);o.$.confirmButton.disabled=$==""||k,a.error=k,a.errorText=k?y??"The value already exists":""}}
+				@keypress=${a=>{a.key==="Enter"&&o.$.confirmButton.click()}}
+				value=${s??""}
+			></${i}>`},cancelButton:{},confirmButton:{buttonType:(e==null?void 0:e.buttonType)??"md-filled-button",label:(e==null?void 0:e.label)??"Confirm",async callback(o){if(e!=null&&e.callback){const i=await e.callback(o);if(i)return i}return o.$.textfield.value}},async onDialogReady(o){await o.updateComplete,o.$.confirmButton.disabled=o.$.textfield.value.length===0}})}async function V({headline:l="Are you sure?",content:s="Are you sure to perform this action?",cancelButton:t={},confirmButton:e={}}={}){return await x({headline:l,content(){return s},confirmButton:e,cancelButton:t})}export{V as materialConfirm,x as materialDialog,R as materialPrompt};
